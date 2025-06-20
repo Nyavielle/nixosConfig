f2dca3481d4bd1f98bda2f05c6a71c847ea008e0
Nyavielle <nyavielle@gmail.com>
2025-06-20 23:46:47 +0300

    update

--- config/hosts/LatitudeE5570/settings/network.nix
+++ config/hosts/LatitudeE5570/settings/network.nix
@@ -4,12 +4,11 @@
   networking.hostName = hostname;
 
   # Network Manager
-  networking.networkmanager.enable = true;
-  networking.wireless.iwd.enable = false;
+  networking.wireless.iwd.enable = true;
 
   # DNS
   services.resolved = {
-    enable = false;
+    enable = true;
     extraConfig = ''
       DNS=1.1.1.1 1.0.0.1
       FallbackDNS=9.9.9.9
@@ -26,29 +25,10 @@
 
   # Firewall
   networking.firewall = {
-    enable = false;
-    allowedTCPPorts = [ 5555 80 443 ];
-    allowedUDPPorts = [ 53 67 68 ];
+    enable = true;
+    allowedTCPPorts = [ 22 ];
+    allowedUDPPorts = [];
     allowPing = false;
     checkReversePath = true;
-  };
-
-  # nftables
-  networking.nftables = {
-    enable = false;
-    ruleset = ''
-      table ip nat {
-        chain POSTROUTING {
-          type nat hook postrouting priority 100;
-          oifname "wlan0" masquerade;
-        }
-      }
-      table ip filter {
-        chain FORWARD {
-          iifname "waydroid0" oifname "wlan0" accept;
-          iifname "wlan0" oifname "waydroid0" ct state related,established accept;
-        }
-      }
-    '';
   };
 }
